d3cd894d08fee41735ba500bdf7e229c9536075d
UJJWAL <ujjwal8851114020@gmail.com>
2025-04-09 15:51:30 +0530

    Navbar and home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
   return (
-    <div>Home Page</div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered practice and feedback</h2>
+          <p className="text-lg">Practice on real interview questions & get instant feedback</p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
 
-export default page
+        </div>
+
+      </section>
+    </>
+  );
+};
+
+export default page;

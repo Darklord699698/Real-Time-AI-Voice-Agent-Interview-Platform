2fdc87c7eb5cf57bdf64b872176ed107be5ac55b
UJJWAL <ujjwal8851114020@gmail.com>
2025-04-09 22:54:17 +0530

    cards details done

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,4 +1,5 @@
-import { getTechLogos } from '@/lib/utils'
+import { cn, getTechLogos } from '@/lib/utils'
+import Image from 'next/image';
 import React from 'react'
 
 const DisplayTechIcons = async({techStack}: {TechIconProps}) => {
@@ -6,8 +7,12 @@ const DisplayTechIcons = async({techStack}: {TechIconProps}) => {
 
   return (
     <div className='flex flex-row '>{techIcons.slice(0,3).map(({tech,url},index)=>(
-        <div className='relative group bg-dark-300 rounded-full p-2 flex-center'>
+        <div key={tech} className={cn('relative group bg-dark-300 rounded-full p-2 flex-center',
+         index >=1 && '-ml-3')}>
             <span className='tech-tooltip'>{tech}</span>
+            <Image src={url} alt={tech} width={100} height={100} 
+            className="size-5"
+            />
         </div>
     ))}
     </div>

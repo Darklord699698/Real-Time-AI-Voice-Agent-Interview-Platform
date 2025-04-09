b9fec4b3df86dca90788e93b1de0e1a7ab2c9565
UJJWAL <ujjwal8851114020@gmail.com>
2025-04-09 22:08:53 +0530

    get Random Interview covers for cards

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,5 +1,7 @@
+import { getRandomInterviewCover } from '@/lib/utils';
 import dayjs from 'dayjs';
-import React from 'react'
+import Image from 'next/image';
+
 const InterviewCard = ({interviewId,userId,role,type,techstack,createdAt}:InterviewCardProps) => {
     const feedback= null as Feedback | null;
     const normalizedType=/mix/gi.test(type) ? 'Mixed' : type;
@@ -11,6 +13,9 @@ const InterviewCard = ({interviewId,userId,role,type,techstack,createdAt}:Interv
                 <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600'>
                     <p className='badge-text'>{normalizedType}</p>
                 </div>
+                <Image src={getRandomInterviewCover()} 
+                alt='cover image' width={90} height={90} 
+                className="rounded-full object-fit size-[90px]"/>
             </div>
         </div>
     </div>

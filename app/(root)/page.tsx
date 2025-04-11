df93ac3a11bc614644da35dd2f8fd192fabb836d
UJJWAL <ujjwal8851114020@gmail.com>
2025-04-11 23:17:30 +0530

    fix reload

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,17 +11,26 @@ const page = () => {
       <section className="card-cta">
         <div className="flex flex-col gap-6 max-w-lg">
           <h2>Get Interview-Ready with AI-Powered practice and feedback</h2>
-          <p className="text-lg">Practice on real interview questions & get instant feedback</p>
+          <p className="text-lg">
+            Practice on real interview questions & get instant feedback
+          </p>
           <Button asChild className="btn-primary max-sm:w-full">
             <Link href="/interview">Start an Interview</Link>
           </Button>
         </div>
-        <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden"/>
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          priority
+          className="max-sm:hidden"
+        />
       </section>
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview)=>(
+          {dummyInterviews.map((interview) => (
             <InterviewCard {...interview} key={interview.id} />
           ))}
         </div>
@@ -29,7 +38,7 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-        {dummyInterviews.map((interview)=>(
+          {dummyInterviews.map((interview) => (
             <InterviewCard {...interview} key={interview.id} />
           ))}
           <p>You haven&apos;t taken any interviews yet</p>

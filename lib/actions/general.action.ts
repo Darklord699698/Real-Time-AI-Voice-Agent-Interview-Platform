6fc232e5554e85259b834e4e625e6e085e48a784
UJJWAL <ujjwal8851114020@gmail.com>
2025-04-13 01:25:10 +0530

    show feedback

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -90,5 +90,25 @@ export async function createFeedback(params: CreateFeedbackParams){
 
     }catch(e){
         console.error("Error saving feedback",e)
+
+        return {success:false}
     }
 }
+
+export async function getFeedbackByInterviewId(
+    params: GetFeedbackByInterviewIdParams
+  ): Promise<Feedback | null> {
+    const { interviewId, userId } = params;
+  
+    const querySnapshot = await db
+      .collection("feedback")
+      .where("interviewId", "==", interviewId)
+      .where("userId", "==", userId)
+      .limit(1)
+      .get();
+  
+    if (querySnapshot.empty) return null;
+  
+    const feedbackDoc = querySnapshot.docs[0];
+    return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
+  }

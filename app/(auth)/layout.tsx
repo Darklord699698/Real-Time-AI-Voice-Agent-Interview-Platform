46be0733e9953beb41db33e4b1b672b1054a785a
UJJWAL <ujjwal8851114020@gmail.com>
2025-04-13 02:32:31 +0530

    fixed bugs

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,13 @@
-import { isAuthenticated } from '@/lib/actions/auth.action';
-import { redirect } from 'next/navigation';
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-const Authlayout = async({children} : {children: ReactNode}) => {
-  const isUserAuthenticated= await  isAuthenticated();
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
-  if(isUserAuthenticated) redirect('/');
-  return (
-    <div className="auth-layout">{children}</div>
-  )
-}
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (isUserAuthenticated) redirect("/");
 
-export default Authlayout
+  return <div className="auth-layout">{children}</div>;
+};
+
+export default AuthLayout;
--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import {
-  getFeedbackByInterviewId, getInterviewsById,
+  getFeedbackByInterviewId,
+  getInterviewById,
 } from "@/lib/actions/general.action";
 import { Button } from "@/components/ui/button";
 import { getCurrentUser } from "@/lib/actions/auth.action";
@@ -13,7 +14,7 @@ const Feedback = async ({ params }: RouteParams) => {
   const { id } = await params;
   const user = await getCurrentUser();
 
-  const interview = await getInterviewsById(id);
+  const interview = await getInterviewById(id);
   if (!interview) redirect("/");
 
   const feedback = await getFeedbackByInterviewId({
